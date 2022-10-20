588d04ae4348296a3879052d83f2844019882eb9
Dhruv Goyal <dungeoninja@gmail.com>
2022-10-20 19:15:29 +0530

    User can add a note and can view the notes along with Date Created

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    noteTitle = models.CharField(max_length=30)
+    noteDesc = models.TextField(max_length=200)
+    time= models.DateTimeField(auto_now_add= True) 
+
+    def __str__(self):
+        return self.noteTitle
--- base/views.py
+++ base/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, HttpResponse
+from base.models import Note
 
 # Create your views here.
 
 def home(request):
+    if request.method=="POST":
+        title=request.POST['title']
+        desc =request.POST['description']
+        obj= Note(noteTitle = title, noteDesc=desc)
+        obj.save()
     return(render(request,'home.html'))
 
+
 def list(request):
-    return(render(request,'list.html'))
+    allNotes=Note.objects.all()
+    print(allNotes)
+    list={'list': allNotes}
+    return(render(request,'list.html', list))
+ 
